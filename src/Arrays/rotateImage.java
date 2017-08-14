3050ada471a638c162cd29e76c3a9a918a262c3b
Joe Kramer <joekramer1125@gmail.com>
2017-08-14 11:06:11 -0700

    add rotateImage

---
+++ src/Arrays/rotateImage.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+
+public class rotateImage {
+    public int[][] rotateImage(int[][] a) {
+        int size = a.length;
+        int[][] b = new int[size][size];
+        for(int i = 0; i < a.length; i++) {
+            int y = size - 1;
+            size--;
+            for(int j = 0; j < a.length; j++) {
+
+                b[i][j] = a[i + y][i];
+                y--;
+            }
+        }
+
+        return b;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         //firstDuplicate
-        int[] a = {2, 3, 3, 1, 5, 2};
+        int[] array1 = {2, 3, 3, 1, 5, 2};
         firstDuplicate arraySolution1 = new firstDuplicate();
-        int answer1 = arraySolution1.firstDuplicate(a);
+        int answer1 = arraySolution1.firstDuplicate(array1);
         System.out.println(answer1);
 
         //firstNotRepeatingCharacter
@@ -15,6 +15,14 @@ public class Main {
         firstNotRepeatingCharacter arraySolution2 = new firstNotRepeatingCharacter();
         char answer2 = arraySolution2.firstNotRepeatingCharacter(s);
         System.out.println(answer2);
+
+        //rotateImage
+        int[][] array3 = {{1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
+        rotateImage arraySolution3 = new rotateImage();
+        int[][] answer3 = arraySolution3.rotateImage(array3);
+        System.out.println(answer3);
 
         //isCryptSolution
         String[] crypt2 = {"SEND", "MORE", "MONEY"};
